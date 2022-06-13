858d2dc59a246c9a4919e1bef178e9f19949c8be
luandino <rosatrepadora2526>
2022-06-12 23:24:12 -0300

    primera actualizacion

--- gestionConsumos/models.py
+++ gestionConsumos/models.py
@@ -4,17 +4,5 @@ from django.db import models
 
 
 class Proveedor(models.Model):
-    nombre = models.CharField(max_length=30)
-    direccion = models.CharField(max_length=100)
-    email = models.EmailField()
-    telefono = models.CharField(max_length=10)
-
-
-class Factura(models.Model):
-    tipo = models.CharField(max_length=2)
-    local = models.IntegerField()
-    numero = models.IntegerField()
-    fecha = models.DateField()
-    entregado = models.BooleanField()
-
+    nombre=models.CharField(max_length=30)
 
